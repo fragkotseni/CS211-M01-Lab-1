58987954067907f6676f95de79f08d4ac8df854a
fragkotseni <tsenifragko@myocu.oak.edu>
2025-08-17 21:52:04 -0500

    Commit Version 1
    Commit Version 1

--- M01-Lab1.py
+++ M01-Lab1.py
@@ -22,7 +22,7 @@ Initialize total order amount to 0
 LOOP (repeat until the customer is done ordering):
     Display the menu with item numbers and prices
     Ask the customer to enter the item number they want to order
-    Ask the customer to enter the quantity for that item
+    Ask the customer to  geenter the quantity for that item
     Calculate the cost for this item:
     item cost = price of item * quantity
     Add the item cost to the total order amount
@@ -33,3 +33,40 @@ Display the total order amount to the customer
 Display a thank you message
 END
 """
+name = input("Hello what's your name? \n")
+print("Hello " + name + " ,welcome to our restaurant!")
+print("Our menu: ")
+
+menu = {
+    1: ("Burger", 5),
+    2: ("Pizza", 8),
+    3: ("Salad", 4),
+    4: ("Soda", 2)
+}
+
+total = 0  
+
+while True:
+    for number, (item, price) in menu.items():
+        print(f"{number}. {item} - ${price}")
+
+    choice = int(input("Enter the item number you want to order: "))
+    if choice < 1 or choice > 4:
+        print("Invalid choice. Try again.")
+        continue
+
+    quantity = int(input(f"How many would you like? "))
+
+    cost = menu[choice][1] * quantity
+    total += cost
+    print(f"Added {quantity} x {menu[choice][0]} = ${cost}")
+
+    more = input("Do you want to order another item? (yes/no): ").lower()
+    if more != "yes/no":
+        print ("Invalid Choice!")
+        continue
+    elif more != "yes":
+        break
+
+print(f"\nYour total order amount is: ${total}")
+print("Thank you for your order!")
